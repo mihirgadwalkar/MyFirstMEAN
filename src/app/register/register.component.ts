2544cb54f24f4a6d02766b6da8a69a7fcdbf023f
mihir <mihirgadwalkar@gmail.com>
2021-06-27 16:09:29 +0530

    first commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us:UserService,private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  onSignup(userObj){
+    this.us.createUser(userObj).subscribe(
+      res=>{
+        if(res.message==="User created"){
+          alert("User created")
+          //navigate to login component
+            this.router.navigateByUrl("/login")
+        }
+        else{
+          alert(res.message)
+        }
+      },
+      err=>{
+        console.log(err)
+        alert("Something went wrong in user creation")
+      }
+    )
+  }
 }
